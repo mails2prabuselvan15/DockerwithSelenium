2cc4b4037bef8395d5c69f973872b5ee1221e16b
Prabu <mails2prabuselvan15@gmail.com>
2020-10-14 10:54:15 +0530

    Docker with Selenium

---
+++ src/test/java/dockerDemo/ChromeDemo.java
@@ -0,0 +1,30 @@
+package dockerDemo;
+
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class ChromeDemo {
+	@Test
+	public void ChromeExecution() throws MalformedURLException, InterruptedException {
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName(BrowserType.CHROME);
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4546/wd/hub"),cap);
+		
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Learn Docker");
+		
+		Thread.sleep(5000);
+		
+		driver.quit();
+	}
+
+}
